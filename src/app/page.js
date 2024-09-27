d222af44b75cb7b0a6881476a2e0c37c1c0616b5
Ahmed <Ahmadmeamar7@gmail.com>
2024-09-27 22:46:39 +0200

    let's go les gars

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="h-screen flex justify-center items-center">
-      <p className="text-4xl md:text-9xl">Let&apos;s go !!!!</p>
+      <p className="text-4xl md:text-9xl">Let&apos;s go les gars !!!!</p>
     </div>
   );
 }
