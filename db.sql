e8c8f602470d64d62863ae7b330c12782be3624b
Gaëtan-Coulombier <gaetanclb@gmail.com>
2024-10-14 18:54:31 +0200

    Ajout des routes api

--- db.sql
+++ db.sql
@@ -1,22 +1,23 @@
 CREATE TABLE `Workspace` (
-  `id` INTEGER PRIMARY KEY,
-  `name` TEXT,
-  `created_at` DATE,
+  `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `name` TEXT NOT NULL,
+  `created_at` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` DATE,
-  `path` TEXT
+  `path` TEXT NOT NULL
 );
 
 CREATE TABLE `User` (
-  `id` INTEGER PRIMARY KEY,
-  `username` TEXT,
-  `password` TEXT,
-  `created_at` DATE,
-  `updated_at` DATE
+  `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `username` TEXT NOT NULL,
+  `password` TEXT NOT NULL,
+  `is_admin` BOOLEAN DEFAULT false,
+  `created_at` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATE 
 );
 
 CREATE TABLE `Role` (
-  `id` INTEGER PRIMARY KEY,
-  `name` TEXT,
+  `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `name` TEXT NOT NULL,
   `description` TEXT,
   `can_read` BOOLEAN DEFAULT false,
   `can_write` BOOLEAN DEFAULT false,
@@ -24,20 +25,20 @@ CREATE TABLE `Role` (
 );
 
 CREATE TABLE `UserRole` (
-  `id` INTEGER PRIMARY KEY,
-  `user_id` INTEGER,
-  `role_id` INTEGER,
-  `workspace_id` INTEGER,
-  `assigned_at` DATE
+  `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `user_id` INTEGER NOT NULL,
+  `role_id` INTEGER NOT NULL,
+  `workspace_id` INTEGER NOT NULL,
+  `assigned_at` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `Invitation` (
-  `id` INTEGER PRIMARY KEY,
-  `workspace_id` INTEGER,
-  `owner_id` INTEGER,
-  `shared_with_id` INTEGER,
-  `role_id` INTEGER,
-  `shared_at` DATE
+  `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `workspace_id` INTEGER NOT NULL,
+  `owner_id` INTEGER NOT NULL,
+  `shared_with_id` INTEGER NOT NULL,
+  `role_id` INTEGER NOT NULL,
+  `shared_at` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 ALTER TABLE `UserRole` ADD FOREIGN KEY (`user_id`) REFERENCES `User` (`id`);
