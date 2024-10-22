c311c8063fef46cb233921b6c2e5b2b2a0db8a8e
PRIOU Antoine <antoine.priou2002@gmail.com>
2024-10-22 23:56:17 +0200

    Start loading list of periods/flags demo

--- src/app/team-graphes/components/BaseApp.js
+++ src/app/team-graphes/components/BaseApp.js
@@ -1,7 +1,10 @@
 "use client"; // Ensure this component is a Client Component
 
 import React, { useState, useEffect, useRef } from 'react';
+import dynamic from 'next/dynamic';
 import Papa from 'papaparse';
+
+const Plotly = require('plotly.js/dist/plotly.js');
 
 
 const BaseApp = () => {
@@ -12,7 +15,6 @@ const BaseApp = () => {
 
     const plotRef1 = useRef(null);
     const plotRef2 = useRef(null);
-    const plotRef3 = useRef(null);
 
     // Function to parse CSV and extract data
     const parseCSV = (file) => {
@@ -49,9 +51,8 @@ const BaseApp = () => {
                 const signalZ = newData.map(row => row.x); // Correctly map to row.z
 
                 setData([
-                    { x: newTimestamps, y: signalX, type: 'scatter', mode: 'lines', line: { color: 'red' } },
-                    { x: newTimestamps, y: signalY, type: 'scatter', mode: 'lines', line: { color: 'green' } },
-                    { x: newTimestamps, y: signalZ, type: 'scatter', mode: 'lines', line: { color: 'blue' } }
+                    { x: newTimestamps, y: signalX, type: 'scatter', mode: 'lines', line: { color: '#FF3131' } },
+                    { x: newTimestamps, y: signalY, type: 'scatter', mode: 'lines', line: { color: '#00BF63' } }
                 ]);
                 setError('');
             },
@@ -82,12 +83,10 @@ const BaseApp = () => {
     }
 
     function setMode(newDragMode) {
-        const Plotly = require('plotly.js/dist/plotly.js');
         console.log(`Mode set to: ${newDragMode}`);
 
         Plotly.relayout(plotRef1.current, { dragmode: newDragMode });
         Plotly.relayout(plotRef2.current, { dragmode: newDragMode });
-        Plotly.relayout(plotRef3.current, { dragmode: newDragMode });
     }
 
     function changeMode(currentMode) {
@@ -101,26 +100,68 @@ const BaseApp = () => {
     }
 
     function resetEvents() {
-        const Plotly = require('plotly.js/dist/plotly.js');
 
         Plotly.relayout(plotRef1.current, { shapes: [], annotations: [] });
         Plotly.relayout(plotRef2.current, { shapes: [], annotations: [] });
-        Plotly.relayout(plotRef3.current, { shapes: [], annotations: [] });
         //setSelections([]);
         //resetMode();
     }
 
     function voidPlots() {
-        const Plotly = require('plotly.js/dist/plotly.js');
         Plotly.purge(plotRef1.current);
         Plotly.purge(plotRef2.current);
-        Plotly.purge(plotRef3.current);
         setData([]);
         setSelections([]);
         setError('');
     }
 
-    function highlightRegion(Plotly, start, end) {
+    function loadFile() {
+
+        console.log('Loading preset periods');
+
+        // timestamp start : 1711113095188
+        // timestamp end : 1711113393011
+
+        const minSignal = 1711113095188;
+        const maxSignal = 1711113393011;
+        const dif = maxSignal - minSignal;
+        const gap = dif / 15;
+
+
+        const exampleRawData = [
+            [1711113095188, 1711113115042],
+            [1711113154752, 1711113174606],
+            [1711113214317, 1711113234171],
+            [1711113273881, 1711113293735],
+            [1711113333446, 1711113353300]
+
+        ];
+
+        const exampleFlagsData = [
+            1711113125042,
+            1711113184752,
+            1711113244317,
+            1711113303881,
+        ];
+
+        const exampleData = [];
+
+        for (let i = 0; i < exampleRawData.length; i++) {
+            highlightRegion(exampleRawData[i][0], exampleRawData[i][1]);
+        }
+
+        for (let i = 0; i < exampleFlagsData.length; i++) {
+            highlightFlag(exampleFlagsData[i]);
+        }
+    }
+
+    function checkPeriods() {
+        const currentPeriodsList = plotRef1.current.layout.shapes;
+        console.log("Current periods: ", currentPeriodsList);
+    }
+
+
+    function highlightRegion(start, end) {
         if (start > end) {
             [start, end] = [end, start];
         }
@@ -133,18 +174,17 @@ const BaseApp = () => {
             y1: 1,
             xref: 'x',
             yref: 'paper',
-            fillcolor: 'rgba(255, 0, 0, 0.35)',
+            fillcolor: 'rgba(255, 222, 89, 0.35)', // Converted hex color #FFDE59 to rgba
             line: {
-                width: 0
+            width: 0
             }
         };
 
         Plotly.relayout(plotRef1.current, { shapes: [...plotRef1.current.layout.shapes, shape] });
         Plotly.relayout(plotRef2.current, { shapes: [...plotRef2.current.layout.shapes, shape] });
-        Plotly.relayout(plotRef3.current, { shapes: [...plotRef3.current.layout.shapes, shape] });
     }
 
-    function highlightFlag(Plotly, xValue) {
+    function highlightFlag(xValue) {
 
         const shape = {
             type: 'line',
@@ -181,19 +221,17 @@ const BaseApp = () => {
             borderpad: 4,
             align: 'left',
             valign: 'middle',
-            
+
         };
 
         Plotly.relayout(plotRef1.current, { shapes: [...plotRef1.current.layout.shapes, shape], annotations: [...plotRef1.current.layout.annotations, annotation] });
         Plotly.relayout(plotRef2.current, { shapes: [...plotRef2.current.layout.shapes, shape], annotations: [...plotRef2.current.layout.annotations, annotation] });
-        Plotly.relayout(plotRef3.current, { shapes: [...plotRef3.current.layout.shapes, shape], annotations: [...plotRef3.current.layout.annotations, annotation] });
     }
 
 
-    function deleteRegion(Plotly, ly_plots, xValue) {
+    function deleteRegion(ly_plots, xValue) {
         const layout_plot1 = ly_plots[0];
         const layout_plot2 = ly_plots[1];
-        const layout_plot3 = ly_plots[2];
 
 
         // Find the region that contains the clicked xValue
@@ -202,26 +240,24 @@ const BaseApp = () => {
             // Remove the region from both layout.shapes and selections
             layout_plot1.shapes.splice(regionIndex, 1);
             layout_plot2.shapes.splice(regionIndex, 1);
-            layout_plot3.shapes.splice(regionIndex, 1);
             selections.splice(regionIndex, 1);
 
             // Update the shapes on the plots
             Plotly.relayout(plotRef1.current, { shapes: layout_plot1.shapes, annotations: layout_plot1.annotations });
             Plotly.relayout(plotRef2.current, { shapes: layout_plot2.shapes, annotations: layout_plot2.annotations });
-            Plotly.relayout(plotRef3.current, { shapes: layout_plot3.shapes, annotations: layout_plot3.annotations });
 
             console.log(`Region removed at x: ${xValue}`);
         }
     }
 
-    const handlePlotClick = (eventData, Plotly) => {
+    const handlePlotClick = (eventData) => {
         const xValue = eventData.points[0].x;
         console.log(`Clicked at x: ${xValue}`);
         console.log(`Current mode: ${mode}`);
 
         // Handle the different modes
         if (mode === 'delete') {
-            deleteRegion(Plotly, [plotRef1.current.layout, plotRef2.current.layout, plotRef3.current.layout], xValue);
+            deleteRegion([plotRef1.current.layout, plotRef2.current.layout], xValue);
         } else {
             if (mode === 'period') {
                 if (selections.length === 0 || selections[selections.length - 1].end !== null) {
@@ -232,13 +268,13 @@ const BaseApp = () => {
                     console.log(`Selected region: Start - ${selections[selections.length - 1].start}, End - ${xValue}`);
                     selections[selections.length - 1].end = xValue;
                     // Highlight the region across all plots
-                    highlightRegion(Plotly, selections[selections.length - 1].start, xValue);
+                    highlightRegion(selections[selections.length - 1].start, xValue);
                 }
             }
 
             if (mode === 'flag') {
                 console.log(`Flag added at x: ${xValue}`);
-                highlightFlag(Plotly, xValue);
+                highlightFlag(xValue);
             }
 
 
@@ -246,7 +282,7 @@ const BaseApp = () => {
     };
 
 
-    const syncZoom = (eventdata, Plotly, [toChange1, toChange2]) => {
+    const syncZoom = (eventdata, [toChange1]) => {
         //get the x and y range of the plot
         const layoutUpdate = {
             'xaxis.range': [eventdata['xaxis.range[0]'], eventdata['xaxis.range[1]']],
@@ -256,7 +292,6 @@ const BaseApp = () => {
         //update the x and y range of the other plots
         if (eventdata['xaxis.range[0]'] !== undefined) {
             Plotly.relayout(toChange1.current, layoutUpdate);
-            Plotly.relayout(toChange2.current, layoutUpdate);
         }
 
         /*
@@ -271,7 +306,8 @@ const BaseApp = () => {
 
 
     useEffect(() => {
-        const Plotly = require('plotly.js/dist/plotly.js'); // Keep your import as is
+
+        console.log('Loading Plotly.js');
 
         // Only create the plots if data is available
         if (data.length > 0) {
@@ -291,21 +327,13 @@ const BaseApp = () => {
                 shapes: [],
                 annotations: [],
                 margin: { t: 40, b: 20, l: 60, r: 20 }
-                
-                
-            };
 
-            const layout_plot3 = {
-                xaxis: { title: 'Timestamp' },
-                yaxis: { title: 'Signal Z' },
-                shapes: [],
-                annotations: [],
-                margin: { t: 40, b: 40, l: 60, r: 20 }
+
             };
 
             const config = {
                 displayModeBar: true,
-                modeBarButtonsToRemove: ['zoom', 'pan', 'toImage', 'sendDataToCloud', 'autoScale2d', 'resetScale2d' ],
+                modeBarButtonsToRemove: ['zoom', 'pan', 'toImage', 'sendDataToCloud', 'autoScale2d', 'resetScale2d'],
                 displaylogo: false,
                 doubleClick: false
             };
@@ -313,18 +341,13 @@ const BaseApp = () => {
 
             Plotly.newPlot(plotRef1.current, [data[0]], layout_plot1, config);
             Plotly.newPlot(plotRef2.current, [data[1]], layout_plot2, config);
-            Plotly.newPlot(plotRef3.current, [data[2]], layout_plot3, config);
 
-            plotRef1.current.on('plotly_click', (eventData) => handlePlotClick(eventData, Plotly));
+            plotRef1.current.on('plotly_click', (eventData) => handlePlotClick(eventData));
             // Add event listener for plotly_click on the second plot
-            plotRef2.current.on('plotly_click', (eventData) => handlePlotClick(eventData, Plotly));
-            // Add event listener for plotly_click on the third plot
-            plotRef3.current.on('plotly_click', (eventData) => handlePlotClick(eventData, Plotly));
+            plotRef2.current.on('plotly_click', (eventData) => handlePlotClick(eventData));
 
-
-            plotRef1.current.on('plotly_relayout', (eventdata) => syncZoom(eventdata, Plotly, [plotRef2, plotRef3]));
-            plotRef2.current.on('plotly_relayout', (eventdata) => syncZoom(eventdata, Plotly, [plotRef1, plotRef3]));
-            plotRef3.current.on('plotly_relayout', (eventdata) => syncZoom(eventdata, Plotly, [plotRef1, plotRef2]));
+            plotRef1.current.on('plotly_relayout', (eventdata) => syncZoom(eventdata, [plotRef2]));
+            plotRef2.current.on('plotly_relayout', (eventdata) => syncZoom(eventdata, [plotRef1]));
 
         }
 
@@ -335,7 +358,6 @@ const BaseApp = () => {
             if (plotRef1.current) {
                 Plotly.purge(plotRef1.current);
                 Plotly.purge(plotRef2.current);
-                Plotly.purge(plotRef3.current);
             }
         };
     }, [data]);
@@ -358,26 +380,28 @@ const BaseApp = () => {
             {/* Conditionally render buttons only when data is loaded */}
             {data.length > 0 && (
                 <div id="controls" style={styles.controls}>
-                    
+
                     <button onClick={() => { setMode('zoom'); }} style={styles.button}>Zoom</button>
                     <button onClick={() => { setMode('pan'); }} style={styles.button}>Pan</button>
                     <br />
 
-                    <button onClick={() => { toggleAddMode(); setMode(false);}} style={styles.button}>Add Period</button>
-                    <button onClick={() => { toggleDeleteMode(); setMode(false) ; }} style={styles.button}>Delete Period</button>
-                    <button onClick={() => { toggleEventMode(); setMode(false) ; }} style={styles.button}>Add Flag</button>
+                    <button onClick={() => { toggleAddMode(); setMode(false); }} style={styles.button}>Add Period</button>
+                    <button onClick={() => { toggleDeleteMode(); setMode(false); }} style={styles.button}>Delete Period</button>
+                    <button onClick={() => { toggleEventMode(); setMode(false); }} style={styles.button}>Add Flag</button>
                     <button onClick={() => { resetMode(); setMode(false); }} style={styles.button}>Reset Mode</button>
                     <br />
-                    <button onClick={() => { resetEvents(); setMode(false) ; }} style={styles.button}>Delete All Periods/Flags</button>
-                    
+                    <button onClick={() => { resetEvents(); setMode(false); }} style={styles.button}>Delete All Periods/Flags</button>
+
                     <span id="modeIndicator" style={styles.modeIndicator}>Mode: {mode}</span>
+                    <button onClick={() => { loadFile(); }} style={styles.button}>Load preset periods</button>
+                    <button onClick={() => { checkPeriods(); }} style={styles.button}>check period list (console)</button>
                 </div>
+
             )}
 
             <div style={styles.plotContainer}>
                 <div ref={plotRef1} style={styles.plot} />
                 <div ref={plotRef2} style={styles.plot} />
-                <div ref={plotRef3} style={styles.plot} />
             </div>
         </div>
     );
