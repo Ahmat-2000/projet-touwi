b591580f1be0b175e98bfae7414a0dd22c331afc
Aref <elaggoun.aref@outlook.fr>
2024-10-30 20:22:38 +0100

    API connexion utilisateur

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,13 +1,18 @@
 import { NextResponse } from 'next/server';
 import { SignJWT } from 'jose';
+import bcrypt from 'bcrypt'; 
 
 export async function POST(request) {
   const { username, password } = await request.json();
 
-  // Vérification des informations d'identification en dur (à des fins de démonstration uniquement)
-  if (username === 'test' && password === 'test') {
-    // Créer un token JWT
-    const token = await new SignJWT({ username })
+  if (!username || !password) {
+    return NextResponse.json({ message: 'Veuillez fournir un nom d\'utilisateur et un mot de passe' }, { status: 400 });
+  }
+
+  // Vérification des informations d'identification
+  if (username == "test" && password == "test") {
+    // Créer un token JWT avec l'ID de l'utilisateur avec payload `sub`
+    const token = await new SignJWT({ sub: 2 })
       .setProtectedHeader({ alg: 'HS256' })
       .setExpirationTime('1h')
       .sign(new TextEncoder().encode(process.env.JWT_SECRET));
