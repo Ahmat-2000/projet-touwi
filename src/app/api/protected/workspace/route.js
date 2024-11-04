69328f91513aa4cc60ca471318f3bfc6ebb2dbe4
NathanFRANCLET <nathanfranclet5@gmail.com>
2024-11-04 14:27:33 +0100

    create workspace via api

--- src/app/api/protected/workspace/route.js
+++ src/app/api/protected/workspace/route.js
@@ -2,6 +2,7 @@
 import { PrismaClient } from '@prisma/client';
 import { workspaceFieldValidations, WorkspaceDTO } from '@/model/workspaceModel';
 import { GenericController } from '@/utils/api/GeneriqueController';
+import { createWorkspace } from '@/services/api/WorkspaceDiskService';
 
 const prisma = new PrismaClient();
 const workspaceController = new GenericController(prisma.workspace, WorkspaceDTO, workspaceFieldValidations);
@@ -13,9 +14,7 @@ export async function GET(request) {
 
 export async function POST(request) {
   const body = await request.json();
-
-  // Create a new path for the workspace
-  body.path = "uuid_workspace"; // TODO : Generate a UUID for the workspace
-
+  const path = await createWorkspace();
+  body.path = path;
   return workspaceController.create({ json: () => Promise.resolve(body) });
 }
