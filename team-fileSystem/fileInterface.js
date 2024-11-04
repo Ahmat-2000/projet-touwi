ad84399f855e0d2a59de3bfedbf93b6dbe084a47
Arthur Lombardo <21911446@etu.unicaen.fr>
2024-11-04 19:51:01 +0100

    added getColumn, comments and tested the whole class, still lacks some features

--- team-fileSystem/fileInterface.js
+++ team-fileSystem/fileInterface.js
@@ -2,38 +2,31 @@ class filesInterface{
     
     // To call with 2 File objects
     constructor(accTimestamps, accX, accY, accZ, gyroTimestamps, gyroX, gyroY, gyroZ, name){
-        this.accTimestamps = accTimestamps;
-        this.accX = accX;
-        this.accY = accY;
-        this.accZ = accZ;
-        this.gyroTimestamps = gyroTimestamps;
-        this.gyroX = gyroX;
-        this.gyroY = gyroY;
-        this.gyroZ = gyroZ;
+        this.accData = {"timestamps": accTimestamps, "x": accX, "y": accY, "z": accZ};
+        this.gyroData = {"timestamps": gyroTimestamps, "x": gyroX, "y": gyroY, "z": gyroZ};
         this.name = name;
     }
 
-    static async initializeFiles(accelFile, gyroFile){
+    static async initializeFiles(accelFile, gyroFile, name=""){
         const [accContent, gyroContent] = await Promise.all([this.readFile(accelFile), this.readFile(gyroFile)]);
-        const { headers: accHeaders, data: accData } = this.processFileContent(accContent);
-        const { headers: gyroHeaders, data: gyroData } = this.processFileContent(gyroContent);
-
+        const { headers: accHeaders, data: accData } = await this.processFileContent(accContent);
+        const { headers: gyroHeaders, data: gyroData } = await this.processFileContent(gyroContent);
 
         // Just basic checks to see if the files are in the correct format
         if (accHeaders.length != 4 || gyroHeaders.length != 4){
-            throw new Error("Invalid file format");
+            throw new Error("Invalid file format, incorrect number of columns");
         }
         if (accHeaders[0] != "timestamp" || gyroHeaders[0] != "timestamp"){
-            throw new Error("Invalid file format");
+            throw new Error("Invalid file format, missing timestamp");
         }
         if (accHeaders[1] != "x" || accHeaders[2] != "y" || accHeaders[3] != "z"){
-            throw new Error("Invalid file format");
+            throw new Error("Invalid file format, missing accel x, y or z");
         }
         if (gyroHeaders[1] != "x" || gyroHeaders[2] != "y" || gyroHeaders[3] != "z"){
-            throw new Error("Invalid file format");
+            throw new Error("Invalid file format, missing gyro x, y or z");
         }
 
-        // Initialize the class variables
+        // Create the variables necessary for the class
         const accTimestamps = gyroData.map(row => row[0]);
         const accX = accData.map(row => row[1]);
         const accY = accData.map(row => row[2]);
@@ -42,11 +35,14 @@ class filesInterface{
         const gyroX = gyroData.map(row => row[1]);
         const gyroY = gyroData.map(row => row[2]);
         const gyroZ = gyroData.map(row => row[3]);
-        const name = accelFile.name + "_" + gyroFile.name;
+        if (name == ""){
+            name = accelFile.name + "_" + gyroFile.name;
+        }
 
         return new filesInterface(accTimestamps, accX, accY, accZ, gyroTimestamps, gyroX, gyroY, gyroZ, name);
     }
 
+    // file is a File object, returns a string with the file content
     static async readFile(file) {
         return new Promise((resolve, reject) => {
             const reader = new FileReader();
@@ -56,10 +52,40 @@ class filesInterface{
         });
     }
 
+    // returns a dictionary with the headers and the data, content is a string with the file content
     static async processFileContent(content) {
         const lines = content.split("\n");
         const headers = lines.shift().split(",");
         const data = lines.map(line => line.split(","));
         return { headers, data };
     }
+
+    // fileName is "accel" or "gyro"
+    // columnName is "timestamps", "x", "y" or "z"
+    // throws an error if the file or column name is invalid
+    // returns the column data
+    getColumn(fileName, columnName){
+        let whichData;
+        if (fileName == "accel"){
+            whichData = this.accData;
+        }
+        else{
+            if (fileName == "gyro"){
+                whichData = this.gyroData;
+            }
+            else{
+                throw new Error("Invalid file name");
+            }
+        }
+        if (columnName != "timestamps" && columnName != "x" && columnName != "y" && columnName != "z"){
+            throw new Error("Invalid column name");
+        }
+        return whichData[columnName];
+    }
+
+    getName(){
+        return this.name;
+    }
 }
+
+export default filesInterface;
