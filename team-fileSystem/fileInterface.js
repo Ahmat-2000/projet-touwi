9d0c6068de84a84c25566f5070d0b7d7fdf2bd50
Arthur Lombardo <21911446@etu.unicaen.fr>
2024-11-05 13:56:57 +0100

    added creation from Chronos file and label column

--- team-fileSystem/fileInterface.js
+++ team-fileSystem/fileInterface.js
@@ -1,13 +1,45 @@
 class filesInterface{
     
     // To call with 2 File objects
-    constructor(accTimestamps, accX, accY, accZ, gyroTimestamps, gyroX, gyroY, gyroZ, name){
-        this.accData = {"timestamps": accTimestamps, "x": accX, "y": accY, "z": accZ};
-        this.gyroData = {"timestamps": gyroTimestamps, "x": gyroX, "y": gyroY, "z": gyroZ};
+    constructor(accTimestamps, accX, accY, accZ, gyroTimestamps, gyroX, gyroY, gyroZ, name, label=undefined){
+        this.accData = {"timestamp": accTimestamps, "x": accX, "y": accY, "z": accZ};
+        this.gyroData = {"timestamp": gyroTimestamps, "x": gyroX, "y": gyroY, "z": gyroZ};
         this.name = name;
+        this.label = label || Array(Math.max(accTimestamps.length, gyroTimestamps.length)).fill("NULL");
     }
 
-    static async initializeFiles(accelFile, gyroFile, name=""){
+    // To call with a File object organized in the Chronos format
+    static async initializeFromChronos(chronosFile, name=""){
+        const content = await this.readFile(chronosFile);
+        const { headers, data } = await this.processFileContent(content);
+
+        // Just basic checks to see if the file is in the correct format
+        if (headers.length != 8){
+            throw new Error("Invalid file format, incorrect number of columns");
+        }
+        if (headers[0] != "timestamp" || headers[1] != "accel_x" || headers[2] != "accel_y" || headers[3] != "accel_z" || headers[4] != "gyro_x" || headers[5] != "gyro_y" || headers[6] != "gyro_z" || headers[7] != "label"){
+            throw new Error("Invalid file format, a column has the wrong name");
+        }
+
+        // Create the variables necessary for the class
+        const timestamps = data.map(row => row[0]);
+        const accX = data.map(row => row[1]);
+        const accY = data.map(row => row[2]);
+        const accZ = data.map(row => row[3]);
+        const gyroX = data.map(row => row[4]);
+        const gyroY = data.map(row => row[5]);
+        const gyroZ = data.map(row => row[6]);
+        const label = data.map(row => row[7]);
+        if (name == ""){
+            name = chronosFile.name;
+        }
+
+        return new filesInterface(timestamps, accX, accY, accZ, timestamps, gyroX, gyroY, gyroZ, name, label);
+
+    }
+
+    // Call this with File objects containing csv files of the timestamp,x,y,z format
+    static async initializeFrom2Csv(accelFile, gyroFile, name=""){
         const [accContent, gyroContent] = await Promise.all([this.readFile(accelFile), this.readFile(gyroFile)]);
         const { headers: accHeaders, data: accData } = await this.processFileContent(accContent);
         const { headers: gyroHeaders, data: gyroData } = await this.processFileContent(gyroContent);
@@ -60,24 +92,28 @@ class filesInterface{
         return { headers, data };
     }
 
-    // fileName is "accel" or "gyro"
-    // columnName is "timestamps", "x", "y" or "z"
+    // fileName is "accel", "gyro" or "label"
+    // columnName is "timestamp", "x", "y" or "z"
     // throws an error if the file or column name is invalid
     // returns the column data
     getColumn(fileName, columnName){
+        fileName = fileName.toLowerCase();
         let whichData;
-        if (fileName == "accel"){
+        if (fileName == "label" || fileName == "labels"){
+            return this.label;
+        }
+        if (fileName == "accel" || fileName == "accelerometer" || fileName == "acc"){
             whichData = this.accData;
         }
         else{
-            if (fileName == "gyro"){
+            if (fileName == "gyro" || fileName == "gyroscope"){
                 whichData = this.gyroData;
             }
             else{
                 throw new Error("Invalid file name");
             }
         }
-        if (columnName != "timestamps" && columnName != "x" && columnName != "y" && columnName != "z"){
+        if (columnName != "timestamp" && columnName != "x" && columnName != "y" && columnName != "z"){
             throw new Error("Invalid column name");
         }
         return whichData[columnName];
@@ -85,6 +121,11 @@ class filesInterface{
 
     getName(){
         return this.name;
+    }
+
+    // is the same as getColumn("label", "whatever")
+    getLabels(){
+        return this.label;
     }
 }
 
